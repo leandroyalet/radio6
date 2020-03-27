ec88e61e81b0d800a326d819cdfa734779030351
Leandro Yalet <leandro@iovlabs.org>
2020-03-27 09:35:30 -0300

    chore: clean up & formatting

--- assets/script/radio6.js
+++ assets/script/radio6.js
@@ -1,87 +1,41 @@
-(function() {
+(function () {
 
-function init() {
+  function init() {
 
-    function fetchSongName(){
+    function fetchSongName() {
       fetch('https://servidorstream.com/radio.php?q=8149/live.xspf')
-      .then(response => response.text())
-      .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-      .then(data => {
-        var title = data.querySelector('playlist > trackList > track > title').textContent;
-        var annotation = data.querySelector('playlist > trackList > track > annotation').textContent;
-        if(title) {
-          document.getElementById('songTitle').innerHTML = title;
-          console.info("Loaded song title: " + title);
-        } else {
-          document.getElementById('songTitle').innerHTML = "AWESOME PIRATE MIX";
-        }
-        if(annotation) {
-          var annotationLines = annotation.split("\n");
-          var currentListeners = annotationLines[4].split(": ");
-          document.getElementById('listeners').innerHTML = currentListeners[0] + ": " + (parseInt(currentListeners[1]) + 53);
-        }
-        setTimeout(function(){
-          fetchSongName();
-        }, 20000);
-      })
+        .then(response => response.text())
+        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+        .then(data => {
+          if (data.querySelector('playlist > trackList > track > title')) {
+            var title = data.querySelector('playlist > trackList > track > title').textContent;
+            var annotation = data.querySelector('playlist > trackList > track > annotation').textContent;
+            if (title) {
+              document.getElementById('songTitle').innerHTML = title;
+              console.info("Loaded song title: " + title);
+            } else {
+              document.getElementById('songTitle').innerHTML = "AWESOME PIRATE MIX";
+            }
+            if (annotation) {
+              var annotationLines = annotation.split("\n");
+              var currentListeners = annotationLines[4].split(": ");
+              document.getElementById('listeners').innerHTML = currentListeners[0] + ": " + (parseInt(currentListeners[1]) + 53);
+            }
+          } else {
+            document.getElementById('songTitle').innerHTML = 'The radio is currently down! Please stay tuned!';
+          }
+          setTimeout(function () {
+            fetchSongName();
+          }, 20000);
+        })
     }
 
     fetchSongName();
-
-  // Demo-specific things: background-switching, etc.
-
-  var bgIndex = 1,
-      backgrounds;
-
-  // note: remote servers will need to serve a CORS response header for cross-domain canvas access.
-  backgrounds = [
-   'https://source.unsplash.com/1600x900/?music,crowd'
-  ];
-
-  /*
-  document.getElementById('nextBackground').onclick = function(e) {
-
-    var i, j, refreshed,
-        loader = document.getElementById('tape-loader');
-
-    function refreshDone() {
-      // re-hide the loader
-      refreshed++;
-      if (refreshed == tapeUIs.length) {
-        if (loader) {
-          loader.className = 'hidden';
-        }
-      }
-    }
-
-    // hack: make the loader visible again
-    if (loader) {
-      loader.className = 'visible';
-    }
-
-    document.getElementsByTagName('html')[0].style.backgroundImage = 'url(' + backgrounds[bgIndex] + ')';
-
-    refreshed = 0;
-
-    for (i=0, j=tapeUIs.length; i<j; i++) {
-      tapeUIs[i].refreshBlurImage(refreshDone);
-    }
-
-    if (++bgIndex >= backgrounds.length) {
-      bgIndex = 0;
-    }
-
-    e.preventDefault();
-    return false;
-
   }
-*/
 
-}
-
-soundManager.setup({
-  onready: init,
-  ontimeout: init
-});
+  soundManager.setup({
+    onready: init,
+    ontimeout: init
+  });
 
 }());
